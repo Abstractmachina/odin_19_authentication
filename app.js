16a77e14999bce41a8d257de5c5a69695b9e1d64
AM_00 <taole.chen@gmail.com>
2023-03-19 17:12:14 +0100

    user login added.

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+require('dotenv').config();
+
 const express = require("express");
 const path = require("path");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
+
 const Schema = mongoose.Schema;
 
-const mongoDb = "YOUR MONGO URL HERE";
+const mongoDb = process.env.MONGODB_AUTH;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
@@ -28,6 +31,57 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
+
+//authentication
+passport.use(
+	new LocalStrategy(async(username, password, done) => {
+		try {
+			const user = await User.findOne({username: username});
+			if (!user) {
+				return done(null, false, {message: "Incorrect username" });
+			}
+			if (user.password !== password) {
+				return done(null, false, {message: "Incorrect passwordd" });
+			}
+			return done(null, user);
+		} catch (err) {
+			return done(err);
+		};
+	})
+);
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(async function(id, done) {
+	try {
+		const user = await User.findById(id);
+		done(null, user);
+	} catch (err) {
+		done(err);
+	}
+});
+
+
+//routes
 app.get("/", (req, res) => res.render("index"));
+app.get('/sign-up', (req, res) => res.render("sign-up-form"));
+app.post('/sign-up', async (req, res, next) => {
+	try {
+		const user = new User({
+			username: req.body.username,
+			password: req.body.password
+		});
+		const result = await user.save();
+		res.redirect('/');
+	} catch (err) {
+		return next(err);
+	}
+});
+
+app.post("log-in", passport.authenticate("local", {
+	successRedirect: "/",
+	failureRedirect: "/",
+}));
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
